ed98d92ae4dbf669504a00c57e3b116973a70ad1
Isaque Marinho Ribeiro <isaque.ribeiro@outlook.com>
2019-09-28 12:01:41 -0300

    Implementando Edição de Perfil do Usuário

--- delphi/appVendaSimples/db/server/script-criar-procedures.sql
+++ delphi/appVendaSimples/db/server/script-criar-procedures.sql
@@ -1,3 +1,6 @@
+USE [venda_simples_server]
+GO
+
 -- =============================================
 -- Author		:	Isaque M. Ribeiro
 -- Create date	:	27/09/2019
@@ -6,6 +9,7 @@
 CREATE or ALTER PROCEDURE dbo.getValidarLogin(
 	@email	 VARCHAR(150)
   , @senha	 VARCHAR(40)
+  , @token	 VARCHAR(40)
   , @id		 VARCHAR(38)  OUT
   , @codigo	 BIGINT		  OUT
   , @nome	 VARCHAR(150) OUT
@@ -20,26 +24,29 @@ BEGIN TRY
   Set @nome		= '';
   Set @retorno	= 'OK';
 
-  Select
-      @id		= u.id_usuario
-	, @codigo	= u.cd_usuario
-	, @nome		= u.nm_usuario
-	, @senhaDB	= u.ds_senha
-  from dbo.sys_usuario u
-  where (u.ds_email = @email);
-
-  If (ISNULL(@codigo, 0) = 0)
-  Begin
-    Set @retorno = 'E-mail inválido';
-  End 
+  if (@token != UPPER(sys.fn_sqlvarbasetostr(HASHBYTES('MD5', concat('TheLordIsGod', convert(varchar(10), getdate(), 103))))))
+    Set @retorno = 'Token Inválido';
   Else
   Begin
-	Set @senhaTMP = Upper(SUBSTRING(sys.fn_sqlvarbasetostr(HASHBYTES('SHA1', @senha)), 3, 40));
+	Select
+		@id		= u.id_usuario
+	  , @codigo	= u.cd_usuario
+	  , @nome		= u.nm_usuario
+	  , @senhaDB	= u.ds_senha
+	from dbo.sys_usuario u
+	where (u.ds_email = @email);
 
-	If ((@senhaTMP = @senhaDB) or (SUBSTRING(@senha, 3, 40) = @senhaDB))
-	  Set @retorno = 'OK';
+	If (ISNULL(@codigo, 0) = 0)
+	  Set @retorno = 'E-mail inválido';
 	Else
-	  Set @retorno = 'E-mail/Senha inválidos'; 
+	Begin
+	  Set @senhaTMP = Upper(SUBSTRING(sys.fn_sqlvarbasetostr(HASHBYTES('SHA1', @senha)), 3, 40));
+
+	  If ((@senhaTMP = @senhaDB) or (SUBSTRING(@senha, 3, 40) = @senhaDB))
+		Set @retorno = 'OK';
+	  Else
+		Set @retorno = 'E-mail/Senha inválidos'; 
+	End
   End
 END TRY
 
@@ -57,6 +64,7 @@ CREATE or ALTER PROCEDURE dbo.setCriarLogin(
     @nome	 VARCHAR(150)
   , @email	 VARCHAR(150)
   , @senha	 VARCHAR(40)
+  , @token	 VARCHAR(40)
   , @id		 VARCHAR(38)  OUT
   , @codigo	 BIGINT		  OUT
   , @retorno VARCHAR(250) OUT)
@@ -68,45 +76,179 @@ BEGIN TRY
   Set @codigo	= 0;
   Set @retorno	= 'OK';
 
-  Select
-      @id		= u.id_usuario
-	, @codigo	= u.cd_usuario
-  from dbo.sys_usuario u
-  where (u.ds_email = @email);
-
-  If (ISNULL(@codigo, 0) > 0)
-  Begin
-    Set @retorno = 'E-mail/Conta já está em uso';
-  End 
+  if (@token != UPPER(sys.fn_sqlvarbasetostr(HASHBYTES('MD5', concat('TheLordIsGod', convert(varchar(10), getdate(), 103))))))
+    Set @retorno = 'Token Inválido';
   Else
   Begin
-	If (UPPER(SUBSTRING(@senha, 1, 2)) <> '0X') 
-	  Set @retorno = 'A Senha não está criptografada'
-	Else
-	If (Trim(@senha) = '')
-	  Set @retorno = 'Senha não informada'
+	Select
+		@id		= u.id_usuario
+	, @codigo	= u.cd_usuario
+	from dbo.sys_usuario u
+	where (u.ds_email = @email);
+
+	If (ISNULL(@codigo, 0) > 0)
+	  Set @retorno = 'E-mail/Conta já está em uso';
 	Else
 	Begin
-	  Set @Id = dbo.ufnGetGuidID();
+	  If (UPPER(SUBSTRING(@senha, 1, 2)) <> '0X') 
+		Set @retorno = 'A Senha não está criptografada'
+	  Else
+	  If (Trim(@senha) = '')
+		Set @retorno = 'Senha não informada'
+	  Else
+	  Begin
+		Set @Id = dbo.ufnGetGuidID();
 
-	  Insert Into dbo.sys_usuario (
-		  id_usuario
-		, nm_usuario
-		, ds_email
-		, ds_senha
-	  ) values (
-		  @id
-		, @nome
-		, LOWER(@email)
-		, UPPER(SUBSTRING(@senha, 3, 40))
-	  );
+		Insert Into dbo.sys_usuario (
+			id_usuario
+		  , nm_usuario
+		  , ds_email
+		  , ds_senha
+		) values (
+			@id
+		  , @nome
+		  , LOWER(@email)
+		  , UPPER(SUBSTRING(@senha, 3, 40))
+		);
 
-	  Set @codigo = SCOPE_IDENTITY();
+		Set @codigo = SCOPE_IDENTITY();
+	  End
 	End
   End
 END TRY
 
 BEGIN CATCH
   Set @retorno = 'Erro ao tentar criar usuário';
+END CATCH
+GO
+
+-- =============================================
+-- Author		:	Isaque M. Ribeiro
+-- Create date	:	28/09/2019
+-- Description	:	Editar Perfil Conta (Login)
+-- =============================================
+CREATE or ALTER PROCEDURE dbo.setEditarPerfilLogin(
+    @id			VARCHAR(38)
+  , @nome		VARCHAR(150)
+  , @email		VARCHAR(150)
+  , @senha		VARCHAR(40)
+  , @cpf_cnpj	VARCHAR(25)
+  , @empresa	VARCHAR(250)
+  , @fantasia	VARCHAR(250)
+  , @token		VARCHAR(40)
+  , @id_empresa	VARCHAR(38)	 OUT
+  , @cd_empresa	INT			 OUT
+  , @retorno	VARCHAR(250) OUT)
+AS
+BEGIN TRY
+  Declare @codigo	BIGINT;
+  Declare @violar	BIGINT;
+  Declare @senhaTMP VARCHAR(40);
+
+  Set @id_empresa	= '{00000000-0000-0000-0000-000000000000}';
+  Set @cd_empresa	= 0;
+  Set @retorno		= 'OK';
+
+  if (@token != UPPER(sys.fn_sqlvarbasetostr(HASHBYTES('MD5', @id))))
+    Set @retorno = 'Token Inválido';
+  Else
+  Begin
+	Select
+		@id		= u.id_usuario
+	  , @codigo	= u.cd_usuario
+	from dbo.sys_usuario u
+	where (u.id_usuario = @id);
+
+	Select
+		@violar = u.cd_usuario
+	from dbo.sys_usuario u
+	where (u.ds_email    = @email)
+	  and (u.id_usuario <> @id);
+
+	If (ISNULL(@codigo, 0) = 0)
+	  Set @retorno = 'Login inválido';
+	Else
+	If (ISNULL(@violar, 0) > 0)
+	  Set @retorno = 'E-mail já utilizado em outra conta';
+	Else
+	Begin
+	  If (UPPER(SUBSTRING(@senha, 1, 2)) <> '0X') 
+		Set @retorno = 'A Senha não está criptografada'
+	  Else
+	  If (Trim(@senha) = '')
+		Set @retorno = 'Senha não informada'
+	  Else
+	  Begin
+	    -- Identificar a Empresa
+		Select
+		    @id_empresa = e.id_empresa
+		  , @cd_empresa = e.cd_empresa
+		from dbo.sys_empresa e
+		where (e.nr_cnpj_cpf = @cpf_cnpj);
+
+		-- Inserir/Editar dados da Empresa
+		If (ISNULL(@cd_empresa, 0) = 0)
+		Begin
+		  Set @id_empresa = dbo.ufnGetGuidID();
+		  
+		  Insert Into dbo.sys_empresa (
+			  id_empresa
+			, nm_empresa
+			, nm_fantasia
+			, nr_cnpj_cpf
+		  ) values (
+			  @id_empresa
+			, @empresa
+			, @fantasia
+			, @cpf_cnpj
+		  );
+
+		  Set @cd_empresa = SCOPE_IDENTITY();
+		End
+		Else
+		Begin
+		  If (LEN(@cpf_cnpj) = 14) -- Apenas "Empressa pessoal" pode ter os dado alterados pela procedure (Empresa com base no CPF formatado)
+		  Begin
+		    Update dbo.sys_empresa Set
+			    nm_empresa	= @empresa
+			  , nm_fantasia	= @fantasia
+			  , nr_cnpj_cpf	= @cpf_cnpj
+		    where (id_empresa = @id_empresa);
+		  End
+		End
+
+		-- Atualizar dados do Usuário
+		Update dbo.sys_usuario Set
+			nm_usuario	= @nome
+		  , ds_email	= @email
+		  , ds_senha	= UPPER(SUBSTRING(@senha, 3, 40))
+		where (id_usuario = @id);
+
+		-- Associar Usuário/Empresa
+		if (not exists(
+		  Select
+		    ue.sn_ativo
+		  from dbo.sys_usuario_empresa ue
+		  where (ue.id_usuario = @id)
+		    and (ue.id_empresa = @id_empresa)
+		))
+		Begin
+		  Insert Into dbo.sys_usuario_empresa (
+			  id_usuario
+			, id_empresa
+			, sn_ativo
+		  ) values (
+			  @id
+			, @id_empresa
+			, (Case when (LEN(@cpf_cnpj) = 14) then 1 else 0 end)
+		  );
+		End
+	  End
+	End
+  End
+END TRY
+
+BEGIN CATCH
+  Set @retorno = 'Erro ao tentar editar perfil do usuário';
 END CATCH
 GO
--- delphi/appVendaSimples/web/webVendaSimples/ws_usuario.asmx.cs
+++ delphi/appVendaSimples/web/webVendaSimples/ws_usuario.asmx.cs
@@ -13,14 +13,29 @@ using webVendaSimples.App_Code;
 namespace webVendaSimples
 {
 
-    class Usuario
+    class Empresa
     {
         public String id;
         public int codigo;
         public String nome;
+        public String fantasia;
+        public String cpf_cnpj;
+
+        public void Destroy()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    class Usuario
+    {
+        public String id;
+        public Int64 codigo;
+        public String nome;
         public String email;
-        public String token;
+        public String token = "";
         public int plataforma = 0;
+        public Empresa empresa = new Empresa();
 
         public String retorno = "";
 
@@ -42,14 +57,15 @@ namespace webVendaSimples
             return "Olá, Mundo";
         }
 
-        // VALIDAR LOGIN
+        // VALIDAR LOGIN ===========================================================================
         [WebMethod]
-        public void ValidarLogin(String email, String senha) {
+        public void ValidarLogin(String email, String senha, String token) {
             Conexao conn = Conexao.Instance;
             List<Usuario> arr = new List<Usuario>();
 
             email = HttpUtility.UrlDecode(email);
             senha = HttpUtility.UrlDecode(senha);
+            token = HttpUtility.UrlDecode(token);
 
             try {
                 conn.Conectar();
@@ -57,10 +73,11 @@ namespace webVendaSimples
 
                 String sql = 
                     "SET DATEFORMAT DMY " +
-                    "EXECUTE dbo.getValidarLogin @email, @senha, @id  OUT, @codigo OUT, @nome OUT, @retorno OUT";
+                    "EXECUTE dbo.getValidarLogin @email, @senha, @token, @id  OUT, @codigo OUT, @nome OUT, @retorno OUT";
 
                 cmd.Parameters.Add(new SqlParameter("@email", email));
                 cmd.Parameters.Add(new SqlParameter("@senha", senha));
+                cmd.Parameters.Add(new SqlParameter("@token", token));
 
                 cmd.Parameters.Add(new SqlParameter("@id", ""));
                 cmd.Parameters["@id"].Direction = ParameterDirection.InputOutput;
@@ -84,7 +101,7 @@ namespace webVendaSimples
                 Usuario usr = new Usuario();
 
                 usr.id = Server.HtmlEncode(cmd.Parameters["@id"].Value.ToString());
-                usr.codigo = int.Parse(cmd.Parameters["@codigo"].Value.ToString());
+                usr.codigo = Int64.Parse(cmd.Parameters["@codigo"].Value.ToString());
                 usr.nome = Server.HtmlEncode(cmd.Parameters["@nome"].Value.ToString());
                 usr.email = email;
                 usr.retorno = Server.HtmlEncode(cmd.Parameters["@retorno"].Value.ToString());
@@ -120,9 +137,9 @@ namespace webVendaSimples
             Context.Response.End();
         }
 
-        // CRIAR NOVA CONTA
+        // CRIAR NOVA CONTA ========================================================================
         [WebMethod]
-        public void CriarLogin(String nome, String email, String senha)
+        public void CriarLogin(String nome, String email, String senha, String token)
         {
             Conexao conn = Conexao.Instance;
             List<Usuario> arr = new List<Usuario>();
@@ -130,6 +147,7 @@ namespace webVendaSimples
             nome  = HttpUtility.UrlDecode(nome);
             email = HttpUtility.UrlDecode(email);
             senha = HttpUtility.UrlDecode(senha);
+            token = HttpUtility.UrlDecode(token);
 
             try
             {
@@ -138,11 +156,12 @@ namespace webVendaSimples
 
                 String sql =
                     "SET DATEFORMAT DMY " +
-                    "EXECUTE dbo.setCriarLogin @nome, @email, @senha, @id  OUT, @codigo OUT, @retorno OUT";
+                    "EXECUTE dbo.setCriarLogin @nome, @email, @senha, @token, @id  OUT, @codigo OUT, @retorno OUT";
 
                 cmd.Parameters.Add(new SqlParameter("@nome",  nome));
                 cmd.Parameters.Add(new SqlParameter("@email", email));
                 cmd.Parameters.Add(new SqlParameter("@senha", senha));
+                cmd.Parameters.Add(new SqlParameter("@token", token));
 
                 cmd.Parameters.Add(new SqlParameter("@id", ""));
                 cmd.Parameters["@id"].Direction = ParameterDirection.InputOutput;
@@ -162,7 +181,7 @@ namespace webVendaSimples
                 Usuario usr = new Usuario();
 
                 usr.id = Server.HtmlEncode(cmd.Parameters["@id"].Value.ToString());
-                usr.codigo = int.Parse(cmd.Parameters["@codigo"].Value.ToString());
+                usr.codigo = Int64.Parse(cmd.Parameters["@codigo"].Value.ToString());
                 usr.nome = nome;
                 usr.email = email;
                 usr.retorno = Server.HtmlEncode(cmd.Parameters["@retorno"].Value.ToString());
@@ -183,6 +202,104 @@ namespace webVendaSimples
                 err.codigo = 0;
                 err.nome = nome;
                 err.email = email;
+                err.retorno = Server.HtmlEncode("ERRO - " + e.Message);
+                arr.Add(err);
+
+                //err = null;
+                GC.SuppressFinalize(err);
+            }
+
+            // Serializar JSON
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            Context.Response.Clear();
+            Context.Response.Write(js.Serialize(arr));
+            Context.Response.Flush();
+            Context.Response.End();
+        }
+
+        // EDITAR PERFIL CONTA =====================================================================
+        [WebMethod]
+        public void EditarPerfilLogin(String id, String nome, String email, String senha, String cpf_cnpj, String empresa, String fantasia, String token)
+        {
+            Conexao conn = Conexao.Instance;
+            List<Usuario> arr = new List<Usuario>();
+
+            id = HttpUtility.UrlDecode(id);
+            nome = HttpUtility.UrlDecode(nome);
+            email = HttpUtility.UrlDecode(email);
+            senha = HttpUtility.UrlDecode(senha);
+            cpf_cnpj = HttpUtility.UrlDecode(cpf_cnpj);
+            empresa = HttpUtility.UrlDecode(empresa);
+            fantasia = HttpUtility.UrlDecode(fantasia);
+            token = HttpUtility.UrlDecode(token);
+
+            try
+            {
+                conn.Conectar();
+                SqlCommand cmd = new SqlCommand("", conn.Conn());
+
+                String sql =
+                    "SET DATEFORMAT DMY " +
+                    "EXECUTE dbo.setEditarPerfilLogin @id, @nome, @email, @senha, @cpf_cnpj, @empresa, @fantasia, @token, @id_empresa OUT, @cd_empresa OUT, @retorno OUT";
+
+                cmd.Parameters.Add(new SqlParameter("@id", id));
+                cmd.Parameters.Add(new SqlParameter("@nome", nome));
+                cmd.Parameters.Add(new SqlParameter("@email", email));
+                cmd.Parameters.Add(new SqlParameter("@senha", senha));
+                cmd.Parameters.Add(new SqlParameter("@cpf_cnpj", cpf_cnpj));
+                cmd.Parameters.Add(new SqlParameter("@empresa", empresa));
+                cmd.Parameters.Add(new SqlParameter("@fantasia", fantasia));
+                cmd.Parameters.Add(new SqlParameter("@token", token));
+
+                cmd.Parameters.Add(new SqlParameter("@id_empresa", ""));
+                cmd.Parameters["@id_empresa"].Direction = ParameterDirection.InputOutput;
+                cmd.Parameters["@id_empresa"].Size = 38;
+
+                cmd.Parameters.Add(new SqlParameter("@cd_empresa", "0"));
+                cmd.Parameters["@cd_empresa"].Direction = ParameterDirection.InputOutput;
+                cmd.Parameters["@cd_empresa"].Size = 10;
+
+                cmd.Parameters.Add(new SqlParameter("@retorno", ""));
+                cmd.Parameters["@retorno"].Direction = ParameterDirection.InputOutput;
+                cmd.Parameters["@retorno"].Size = 250;
+
+                cmd.CommandText = sql;
+                SqlDataReader qry = cmd.ExecuteReader();
+
+                Usuario usr = new Usuario();
+
+                usr.id = Server.HtmlEncode(cmd.Parameters["@id"].Value.ToString());
+                usr.nome = nome;
+                usr.email = email;
+                usr.empresa.id = Server.HtmlEncode(cmd.Parameters["@id_empresa"].Value.ToString());
+                usr.empresa.codigo = int.Parse(cmd.Parameters["@cd_empresa"].Value.ToString());
+                usr.empresa.nome = empresa;
+                usr.empresa.fantasia = fantasia;
+                usr.empresa.cpf_cnpj = cpf_cnpj;
+                usr.retorno = Server.HtmlEncode(cmd.Parameters["@retorno"].Value.ToString());
+                arr.Add(usr);
+
+                //usr = null;
+                //cmd = null;
+                GC.SuppressFinalize(usr);
+                GC.SuppressFinalize(cmd);
+
+                conn.Fechar();
+            }
+            catch (System.IndexOutOfRangeException e)
+            {
+                Usuario err = new Usuario();
+
+                err.id = "{00000000-0000-0000-0000-000000000000}";
+                err.codigo = 0;
+                err.nome = nome;
+                err.email = email;
+                err.empresa.id = "{00000000-0000-0000-0000-000000000000}";
+                err.empresa.codigo = 0;
+                err.empresa.nome = "";
+                err.empresa.fantasia = "";
+                err.empresa.cpf_cnpj = "";
                 err.retorno = Server.HtmlEncode("ERRO - " + e.Message);
                 arr.Add(err);
 
